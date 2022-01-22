b40266eb413b9861b771b8935b3e1a60ee82e570
rdmills <rdmills@users.noreply.github.com>
2022-01-22 18:25:09 +0000

    Add files via upload
    Initial commit

---
+++ langevin.cpp
@@ -0,0 +1,40 @@
+/*
+    langevin.cpp
+    Langevin abstract class for langevin dynamics on a finite interval.
+    @author Rory Mills-Williams
+    @version 1.0 20/01/2022
+*/
+
+#include "langevin.hpp"
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+Langevin::Langevin(opts_num opts1)
+{
+    optsNum = opts1;
+
+    numSteps       = opts1.num_steps;
+    this->SetIntegrator(opts1.integrator);
+    numParticles   = opts1.num_particles;
+    tMax           = opts1.t_max;
+
+    initParticles.resize(numParticles);
+    particles.resize(numParticles);
+
+    for (int i=0; i < particles.size(); i++)
+    {
+        particles[i].resize(numSteps);
+    }
+}
+
+void Langevin::SetIntegrator(std::string integrator)
+{
+    mIntegratorType = integrator;
+}
+
+std::string Langevin::GetIntegrator() const
+{
+    return mIntegratorType;
+}
---
+++ mckean_vlasov.cpp
@@ -0,0 +1,52 @@
+/*
+    mckean_vlasov.cpp
+    Entry point for running langevin dynamics on a finite interval.
+    @author Rory Mills-Williams
+    @version 1.0 20/01/2022
+*/
+
+#include <iostream>
+#include "langevin.hpp"
+#include "line1d.hpp"
+
+int main(int argc, char* argv[])
+{
+    // Choose numerical and physical options.
+    struct opts_num optsNum   = {12,"Euler–Maruyama", 10, 1.0};
+    struct opts_phys optsPhys = {{-0.5,0.5}};
+
+    // Make a 1d line.
+    Line1D aLine(optsNum,optsPhys);
+
+    std::cout<<*(aLine.GetyMinyMax()+1)<<std::endl;
+    std::cout<<aLine.GetIntegrator()<<std::endl;
+
+
+    // Langevin pl(optsNum);
+
+    // pl.InitialUniformParticles();
+
+    // for (int i = 0; i < pl.particles.size(); i++)
+    // {
+    //     for (int j = 0; j < pl.particles[i].size(); j++)
+    //     {
+    //         pl.particles[i][j] = 1.0;
+    //     }   
+    // }
+
+    // for (int i = 0; i < pl.particles.size(); i++)
+    //     {
+    //         for (int j = 0; j < pl.particles[i].size(); j++)
+    //         {
+    //             std::cout << pl.particles[i][j] << " ";
+    //         }   
+    //         std::cout << std::endl;
+    //     }
+
+
+    // std::cout << pl.dW(0.1);
+    // std::cout << pl.dW(0.1);
+
+
+    return 0;
+}
