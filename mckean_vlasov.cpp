c153282e5ac92eddf673433463fd3c7ae8d4ba8d
rdmills <rdmills@users.noreply.github.com>
2022-01-22 19:46:28 +0000

    test commit

--- mckean_vlasov.cpp
+++ mckean_vlasov.cpp
@@ -21,6 +21,7 @@ int main(int argc, char* argv[])
     std::cout<<*(aLine.GetyMinyMax()+1)<<std::endl;
     std::cout<<aLine.GetIntegrator()<<std::endl;
 
+    int j=1;
 
     // Langevin pl(optsNum);
 
