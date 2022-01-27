06c02591e746d732e29864d92cfbf10977e5b3ab
rdmills <rdmills@users.noreply.github.com>
2022-01-27 20:43:59 +0000

    added derived euler maruyama class

--- euler_maruyama.hpp
+++ euler_maruyama.hpp
@@ -16,40 +16,41 @@
 // Optional physics for a 1d line.
 // Example:
 //      struct opts_phys optsPhys = {{-0.5,0.5}}; 
-// struct opts_phys
-//     {
-//         double interval[2];  
-//         double (*pV1)(double);
-//         double (*pGradV1) (double); 
-//         double kappa1;
-//         double (*pV2)(double, double);
-//         double (*pGradV2)(double, double);
-//         double kappa2;
+struct opts_phys
+    {
+        double interval[2];  
+        double (*pV1)(double);
+        double (*pGradV1) (double); 
+        double kappa1;
+        double (*pV2)(double, double);
+        double (*pGradV2)(double, double);
+        double kappa2;
 
-//     };
+    };
 
 // Child class Line1D for langevin dynamics on an interval.
 class EulerMaruyama : public Solver
 {       
     public: 
-        // double yMin;
-        // double yMax;
+        EulerMaruyama(opts_num opts1, opts_phys opts2);
 
-        // opts_phys optsPhys;
+        virtual double RightHandSide(double y, double t); 
 
-        // EulerMaruyama(opts_num opts1, opts_phys opts2);
-        EulerMaruyama(opts_num opts1);
+        // void SetyMinyMax(double interval[2]);
+        // double* GetyMinyMax();
+        // virtual void InitialiseParticles();
         
-        void SetyMinyMax(double interval[2]);
-        double* GetyMinyMax();
-        virtual void InitialiseParticles();
-        
-        virtual void DoLangevin();
-        virtual void GenerateNoiseVector();
-        virtual void PrintNoiseVector();
+        // virtual void DoLangevin();
+        // virtual void GenerateNoiseVector();
+        // virtual void PrintNoiseVector();
 
     
     private:
+        double (*mV1)(double y); 
+        double (*mGradV1)(double y);
+        double (*mV2)(double y, double yPrime); 
+        double (*mGradV2)(double y, double yPrime);
+
         double myMinyMax [2];
         double* mNoiseVector;    
 };
